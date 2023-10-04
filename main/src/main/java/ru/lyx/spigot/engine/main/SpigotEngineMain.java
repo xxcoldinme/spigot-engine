3a0d8ba209afbd6b989749526fe7bf92f1319dc5
mikhail.sterkhov <mikhail.sterkhov@x5.ru>
2023-10-04 13:15:26 +0300

    add main module for total project build & running as plugin

---
+++ main/src/main/java/ru/lyx/spigot/engine/main/SpigotEngineMain.java
@@ -0,0 +1,24 @@
+package ru.lyx.spigot.engine.main;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.lyx.spigot.engine.SpigotEngine;
+
+public final class SpigotEngineMain extends JavaPlugin {
+
+    @Getter
+    private SpigotEngine engine = new SpigotEngine(Bukkit.getServer());
+
+    @Override
+    public void onEnable() {
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new SpigotPluginLoadingObserver(engine), this);
+    }
+
+    @Override
+    public void onDisable() {
+        engine = null;
+    }
+}
---
+++ main/src/main/java/ru/lyx/spigot/engine/main/SpigotPluginLoadingObserver.java
@@ -0,0 +1,42 @@
+package ru.lyx.spigot.engine.main;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.Plugin;
+import ru.lyx.spigot.engine.SpigotEngine;
+import ru.lyx.spigot.engine.plugin.SpigotContextPlugin;
+
+@RequiredArgsConstructor
+public final class SpigotPluginLoadingObserver implements Listener {
+
+    private final SpigotEngine engine;
+
+    @EventHandler
+    public void handle(PluginEnableEvent event) {
+        final Plugin plugin = event.getPlugin();
+
+        if (plugin instanceof SpigotContextPlugin) {
+
+            SpigotContextPlugin spigotContextPlugin = (SpigotContextPlugin) plugin;
+            engine.registerPlugin(spigotContextPlugin);
+
+            spigotContextPlugin.setEngine(engine);
+        }
+    }
+
+    @EventHandler
+    public void handle(PluginDisableEvent event) {
+        final Plugin plugin = event.getPlugin();
+
+        if (plugin instanceof SpigotContextPlugin) {
+
+            SpigotContextPlugin spigotContextPlugin = (SpigotContextPlugin) plugin;
+            engine.unregisterPlugin(spigotContextPlugin);
+
+            spigotContextPlugin.setEngine(null);
+        }
+    }
+}
