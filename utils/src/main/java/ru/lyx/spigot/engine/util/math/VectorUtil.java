bcebc87043dd04cb9591e76d8d96250e48a4d43b
xxcoldinme <misha_stonlex@bk.ru>
2023-10-17 14:27:32 +0300

    add VectorUtil#getLookDirection

--- utils/src/main/java/ru/lyx/spigot/engine/util/math/VectorUtil.java
+++ utils/src/main/java/ru/lyx/spigot/engine/util/math/VectorUtil.java
@@ -6,7 +6,7 @@ import org.bukkit.World;
 import org.bukkit.util.Vector;
 
 @UtilityClass
-public class VectorUtils {
+public class VectorUtil {
 
     public Location applyOffset(Location origin, Vector offset) {
         return origin.clone().add(offset);
@@ -14,5 +14,13 @@ public class VectorUtils {
 
     public Location applyOffset(World world, Vector origin, Vector offset) {
         return applyOffset(origin.toLocation(world), offset);
+    }
+
+    public Vector getLookDirection(Location from, Location to) {
+        return getLookDirection(from.toVector(), to.toVector());
+    }
+
+    public Vector getLookDirection(Vector from, Vector to) {
+        return to.clone().subtract(from).normalize();
     }
 }
